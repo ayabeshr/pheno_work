130e52f0790e6307e7eadb30e00b29dbe1ed3897
AyaBeshr <50970624+ayabeshr@users.noreply.github.com>
2021-08-01 12:52:36 +0200

    Add files via upload
    for using in Centos platforms

--- run_MG_20210730.py
+++ run_MG_20210730.py
@@ -1,21 +1,21 @@
-############################################
+###########################################
 #   script for running MG5 at once for     #
 #   several Benchmark points with varying  # 
 #   parameter cards                        #                                       
 ############################################
 
 import os 
-import subprocess 
+#import subprocess 
 
 #subprocess.run("ls")
 #subprocess.run(["./bin/mg5_aMC", "test2.txt"])
 
 # define mandatory directories 
-mg_dir = "/home/aya/programs/MG5_aMC_v2_7_3"
-scan_dir = "/home/aya/HEP/toolbox-2.0.3/SSP-1.2.5/Output/THDMScan8M12lam2"
+mg_dir = "/home/Aya/MG5_aMC_v2_7_3"
+scan_dir = "/home/Aya/THDMScan8M12Lam2_20210801"
 
 # creat a text file for saving process generation commands 
-tf = open("/home/aya/programs/MG5_aMC_v2_7_3/gghhbb4Mu.txt", "w")
+tf = open("/home/Aya/MG5_aMC_v2_7_3/gghhbb4Mu.txt", "w")
 tf2 = mg_dir + "/gghhbb4Mu.txt"
 
 # check if the text file has been created successfully 
@@ -32,7 +32,7 @@ tf.write("import model 2HDM_20210524 -modelname \n")
 tf.write("define j = g u1 u2 d1 d2 d3 u1bar u2bar d1bar d2bar d3bar \n")
 
 # Iterate over spc files in a given range
-for i in range(27, 101):
+for i in range(101, 501):
 #for i in range(101, 1000):
 #for i in range(1000, 2000):
 #for i in range(2000, 4000):
@@ -42,7 +42,7 @@ for i in range(27, 101):
 	sf = scan_dir + "/SPheno.spc.THDM__" + str(i) 
 	if(os.path.isfile(sf)):
 		tf.write("generate g g > h2 > h1 h1, h1 > d3 d3bar, (h1 > z z, z > e2bar e2, z > e2bar e2) \n")
-		tf.write("output gg_hh_bb4Mu_20210731/gg_hh_bb4Mu_" + str(i) + "\n")
+		tf.write("output gg_hh_bb4Mu_20210801/gg_hh_bb4Mu_" + str(i) + "\n")
 		l = ["launch \n", "0 \n", "set nevents 10 \n", "set ebeam1 7000 \n", "set ebeam2 7000 \n"]
 		tf.writelines(l)
 		tf.write(sf + " \n")
@@ -52,6 +52,10 @@ tf.close()
 print("Writing Txt file ENDS!")
 #subprocess.run(["vim", "gghhbb4Mu.txt"])
 
-subprocess.run(["./bin/mg5_aMC", "gghhbb4Mu.txt"])
-print("Running MG ENDS!") 
-	
+# subprocess used in python3
+#subprocess.run("alias python=python2")
+#subprocess.run(["python", "./bin/mg5_aMC", "gghhbb4Mu.txt"])
+
+# os.system() is used in python2
+os.system("python2 ./bin/mg5_aMC gghhbb4Mu.txt") 
+print("Running MG ENDS!")
