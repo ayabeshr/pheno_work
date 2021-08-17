766ed62cb23f7141deb842b650a2dea847789415
AyaBeshr <50970624+ayabeshr@users.noreply.github.com>
2021-08-17 08:24:39 +0200

    Add files via upload

--- extracting_xsections_mg5.py
+++ extracting_xsections_mg5.py
@@ -8,7 +8,7 @@ import os
 
 prcs_mother_dir = "/home/Aya/MG5_aMC_v2_7_3/gg_hh_bb4Mu_20210801"
 
-for i in range(121, 301):
+for i in range(1501, 1801):
 	prcs_daughter_dir = prcs_mother_dir + "/gg_hh_bb4Mu_" + str(i) 
 	if(os.path.exists(prcs_daughter_dir)):
 		xsc_file = prcs_daughter_dir + "/Events/run_01/run_01_tag_1_banner.txt"
@@ -17,7 +17,7 @@ for i in range(121, 301):
 		lines = xf.readlines() 
 		for line in lines:
 			if line[0:20] == "#  Integrated weight":
-				print("Xsc for Point nb : [" + str(i) + "] is : " + line) 
+				print(line[35:50]) 
 				
 		xf.close()
 		
