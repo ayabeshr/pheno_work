74e9113e57428cadbcf43e12d33628851bbcfe4c
AyaBeshr <50970624+ayabeshr@users.noreply.github.com>
2021-08-17 22:14:17 +0200

    Update make3Dplot.py

--- make3Dplot.py
+++ make3Dplot.py
@@ -7,14 +7,13 @@ import csv
 from matplotlib import pyplot as plt 
 from mpl_toolkits.mplot3d import Axes3D
 
-
 # Define CSV file 
-df = pd.read_csv("/home/aya/Videos/Book2.csv")
+df = pd.read_csv("/home/aya/Desktop/Pheno_Work/analysis/Xsection_Test_20210817_2.csv")
 
 # Define coloumns in csv file in terms of x, y and z for plotting
-x = df['Chi']
-y = df['param']
-z = df['X-section(pb)']
+x = df['csqr_total']
+y = df['mbreaking']
+z = df['xsection(pb)']
 
 # plot data from file 
 
@@ -22,11 +21,11 @@ z = df['X-section(pb)']
 fig = plt.figure(figsize=(7,7))
 my3dplot = fig.add_subplot(111, projection = '3d')
 
-my3dplot.scatter(x, y, z, marker = "o", color = "blue", label = "2HDM-type1\nPhase2 Simulation\n14TeV")     # or you can use my3dplot.scatter(x,y,z) which is equivalent and blue color is default
-
-plt.xlabel("chi.e+02", color = "red")          # or you can equivalently use my3dplot.set_xlabel('chi.e+02', color = "red")
-plt.ylabel("m12.e+02", color = "magenta")      # or you can equivalently use my3dplot.set_ylabel('m12.e+02', color = "magneta")
-my3dplot.set_zlabel('xsection(pb).e-07', color = "green")
+my3dplot.scatter(x, y, z, marker = "o", color = "blue") 
+plt.xlabel(r"$\chi^2$", color="blue")
+plt.ylabel(r"$M12 x 10^2$", color="blue")
+my3dplot.set_zlabel(r"$\sigma(pb)(gg \rightarrow H \rightarrow hh \rightarrow bb4Mu) x 10^{-11}$", color="red")
+my3dplot.view_init(27)
 
 # way to add a legend
-my3dplot.legend(loc='upper left')    # the contents of legend are define in label 
+#my3dplot.legend(loc='upper left')    # the contents of legend are define in label 
