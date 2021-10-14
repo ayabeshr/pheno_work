38214d7ec626a6dfcd633366acf4c25e66827b29
AyaBeshr <50970624+ayabeshr@users.noreply.github.com>
2021-10-14 16:36:05 +0200

    Update extract_xsc_mg5.py

--- Generation_MG5_Samples/extract_xsc_mg5.py
+++ Generation_MG5_Samples/extract_xsc_mg5.py
@@ -1,26 +1,42 @@
-#########################################
+########################################
 #  script for extracting x-sections from
 #  several generated processes by MG
 #########################################
 
+import os, sys 
 
-import os 
 
-prcs_mother_dir = "/home/Aya/MG5_aMC_v2_7_3/gg_hh_bb4Mu_20210801"
+prcs_mother_dir = "/HEP_DATA/aya/signal_20210921_xsc_test"
+xsc = [] 
 
-for i in range(1501, 1801):
+# loop overall prcs daughter directories
+for i in range(400, 410):
+	
 	prcs_daughter_dir = prcs_mother_dir + "/gg_hh_bb4Mu_" + str(i) 
+	
 	if(os.path.exists(prcs_daughter_dir)):
-		xsc_file = prcs_daughter_dir + "/Events/run_01/run_01_tag_1_banner.txt"
-		#with open(xsc_file, "r") as xf:
-		xf = open(xsc_file, "r")
-		lines = xf.readlines() 
-		for line in lines:
-			if line[0:20] == "#  Integrated weight":
-				print(line[35:50]) 
-				
-		xf.close()
 		
-print("...Extracting Xsections Done...")
-print("   ")
+		xsc_file = prcs_daughter_dir + "/Events/run_01/run_01_tag_1_banner.txt"
+		
+		with open(xsc_file) as xf:
+			lines = xf.readlines()
+			for line in lines:
+				if line[0:20] == "#  Integrated weight":
+					xsection = line[35:55]
+					xsc.append(xsection)
+
+# save print output to a txt file
+sys.stdout = open("all_xsc.txt", "w")
+
+leng = len(xsc)		
+i = 0 
+
+while i < leng:
+	print (xsc[i])
+	i += 1
+	
+sys.stdout.close()
+	
+#print("...Extracting Xsections Done...")
+#print("   ")
 	
