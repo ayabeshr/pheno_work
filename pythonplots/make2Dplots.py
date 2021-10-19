dcbf695ad718333b6c851eae5e602eb3c6623e4c
AyaBeshr <50970624+ayabeshr@users.noreply.github.com>
2021-10-19 12:34:46 +0200

    Update make2Dplots.py

--- pythonplots/make2Dplots.py
+++ pythonplots/make2Dplots.py
@@ -9,11 +9,14 @@ from matplotlib import pyplot as plt
 
 # Define CSV file 
 df = pd.read_csv("/home/aya/Desktop/Pheno_Work/analysis/data_plots/Xsection_Test_20211019.csv")
+#df = pd.read_csv("/home/aya/Downloads/Xsection_Test_20210719 - Sheet1(3).csv")
+# plot data from file 
+#m12_xsc = dfl.plot("Mvalue", "X-section(pb)", marker = "o", color = "red", linestyle = "")
 
 # Xsection Vs M12
-xsc_Vsm12 = df.plot(x = "mbreaking", y = "new_xsc(pb)", marker = "o", color = "blue", label =  "$\sigma (pb)$ \n at tree-level", linestyle="")
+xsc_Vsm12 = df.plot(x = "mbreaking", y = "new_xsc(pb)", marker = ".", color = "blue", label =  "$\sigma (pb)$ \n at tree-level", linestyle="")
 plt.grid(True)
-plt.xlabel(r"$M_{12}^{3}$")
+plt.xlabel(r"$M_{12}^{3}$")    # r refers to raw string, placed before string, used when writting math expressions
 plt.ylabel(r"$\sigma(pb)(gg \rightarrow H \rightarrow hh \rightarrow bb4Mu) x 10^{-8}$")
 plt.legend(loc='upper left')
 
@@ -21,20 +24,20 @@ plt.legend(loc='upper left')
 # x = df['coloumn1_name'].iloc[0:10].values
 
 # Xsection Vs Total Chi Square
-xsc_Vschi = df.plot(x = "csqr_total", y = "new_xsc(pb)", marker = "o", color = "red", label =  "$\sigma (pb)$ \n at tree-level", linestyle="")
+xsc_Vschi = df.plot(x = "csqr_total", y = "new_xsc(pb)", marker = ".", fillstyle=None, color = "red", label =  "$\sigma (pb)$ \n at tree-level", linestyle="")
 plt.grid(True)
 plt.xlabel(r"$\chi^{2}$")
 plt.ylabel(r"$\sigma(pb)(gg \rightarrow H \rightarrow hh \rightarrow bb4Mu) x 10^{-8}$")
 plt.legend(loc="upper left")
 
 # H2 mass Vs M12 
-h2mass_Vsm12 = df.plot(x = "mbreaking", y = "mH(GeV)", marker = "o", color = "green", linestyle="")
+h2mass_Vsm12 = df.plot(x = "mbreaking", y = "mH(GeV)", marker = ".", color = "green", linestyle="")
 plt.grid(True)
 plt.xlabel(r"$M_{12}^{3}$")
 plt.ylabel("H_mass(GeV)")
 
 # Xsection Vs H2 mass 
-xsc_Vsh2mass = df.plot(x = "mH(GeV)", y = "new_xsc(pb)", marker = "o", color = "magenta", label =  "$\sigma (pb)$ \n at tree-level", linestyle="")
+xsc_Vsh2mass = df.plot(x = "mH(GeV)", y = "new_xsc(pb)", marker = ".", color = "magenta", label =  "$\sigma (pb)$ \n at tree-level", linestyle="")
 plt.grid(True)
 plt.xlabel("H_mass(GeV)")
 plt.ylabel(r"$\sigma(pb)(gg \rightarrow H \rightarrow hh \rightarrow bb4Mu) x 10^{-8}$")
