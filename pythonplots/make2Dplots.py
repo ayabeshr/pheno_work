024f00430047fd4e251401ad7dd1d7401a86beb9
AyaBeshr <50970624+ayabeshr@users.noreply.github.com>
2021-10-25 17:00:46 +0200

    Update make2Dplots.py

--- pythonplots/make2Dplots.py
+++ pythonplots/make2Dplots.py
@@ -1,5 +1,4 @@
-import os, sys
-import openpyxl
+iimport os, sys
 import array as arr 
 from pathlib import Path
 import pandas as pd
@@ -8,7 +7,7 @@ from matplotlib import pyplot as plt
 
 
 ## Define CSV file 
-df = pd.read_csv("/home/aya/Desktop/Pheno_Work/analysis/data_plots/Xsection_Test_20211020.csv")
+df = pd.read_csv("/home/aya/Xsection_Test_20211020_copy.csv")
 x = df['mbreaking'].values
 y = df['new_xsc(pb)'].values
 xarr = arr.array('d',[-3.8, -3.2])
@@ -21,15 +20,14 @@ xsc_Vsm12 = df.plot(x = "mbreaking", y = "new_xsc(pb)",
                     linestyle="")
 #threshold = 3
 
-plt.xlabel(r"$M_{12}^{3}$")    # r refers to raw string, placed before string, used when writting math expressions
-plt.ylabel(r"$\sigma(gg \rightarrow H \rightarrow hh \rightarrow bb4Mu) x 10^{-8} (pb)$")
+plt.xlabel(r'$M_{12}^{3}$')    # r refers to raw string, placed before string, used when writting math expressions
+plt.ylabel(r"$\sigma(gg\rightarrow$"+"H"+r"$\rightarrow$"+"hh"+r"$\rightarrow$"+"bb4Mu) x"+r"$10^{-8}$"+" (pb)")
 plt.grid(True)
 plt.legend(loc='upper left')
+plt.savefig('/home/aya/Pheno_Work/analysis/data_plots/newplots/xsc_m12.png')
 #plt.axhline(threshold, color = "0", alpha = 1, lw=2, linestyle = "--")
 #plt.fill_between(-y,-0.1,4, where=y > threshold, color='tab:gray', alpha=0.3)
 
-# if you need to plot certain range of a coloumn values, for eg, 1st 10 values do: 
-# x = df['coloumn1_name'].iloc[0:10].values
 
 # Xsection Vs Total Chi Square
 xsc_Vschi = df.plot(x = "csqr_total", y = "new_xsc(pb)", 
@@ -38,9 +36,10 @@ xsc_Vschi = df.plot(x = "csqr_total", y = "new_xsc(pb)",
                     linestyle="",  fillstyle=None, )
 
 plt.xlabel(r"$\chi^{2}$")
-plt.ylabel(r"$\sigma(gg \rightarrow H \rightarrow hh \rightarrow bb4Mu) x 10^{-8} (pb)$")
+plt.ylabel(r"$\sigma(gg\rightarrow$"+"H"+r"$\rightarrow$"+"hh"+r"$\rightarrow$"+"bb4Mu) x"+r"$10^{-8}$"+" (pb)")
 plt.grid(True)
 plt.legend(loc="upper left")
+plt.savefig('/home/aya/Pheno_Work/analysis/data_plots/newplots/xsc_chis.png')
 
 # H2 mass Vs M12 
 h2mass_Vsm12 = df.plot(x = "mbreaking", y = "mH(GeV)", 
@@ -50,6 +49,7 @@ h2mass_Vsm12 = df.plot(x = "mbreaking", y = "mH(GeV)",
 plt.xlabel(r"$M_{12}^{3}$")
 plt.ylabel("H_mass(GeV)")
 plt.grid(True)
+plt.savefig('/home/aya/Pheno_Work/analysis/data_plots/newplots/heavyHigss_m12.png')
 
 # Xsection Vs H2 mass 
 xsc_Vsh2mass = df.plot(x = "mH(GeV)", y = "new_xsc(pb)", 
@@ -58,6 +58,7 @@ xsc_Vsh2mass = df.plot(x = "mH(GeV)", y = "new_xsc(pb)",
                        linestyle="")
 
 plt.xlabel("H_mass(GeV)")
-plt.ylabel(r"$\sigma(gg \rightarrow H \rightarrow hh \rightarrow bb4Mu) x 10^{-8} (pb)$")
+plt.ylabel(r"$\sigma(gg\rightarrow$"+"H"+r"$\rightarrow$"+"hh"+r"$\rightarrow$"+"bb4Mu) x"+r"$10^{-8}$"+" (pb)")
 plt.grid(True)
 plt.legend(loc='upper right')
+plt.savefig('/home/aya/Pheno_Work/analysis/data_plots/newplots/xsc_heavyHiggs.png')
