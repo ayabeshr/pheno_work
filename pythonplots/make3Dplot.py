545196d68e824fa580b98dfe98191ddf9c8207ce
AyaBeshr <50970624+ayabeshr@users.noreply.github.com>
2021-10-28 11:46:50 +0200

    Update make3Dplot.py

--- pythonplots/make3Dplot.py
+++ pythonplots/make3Dplot.py
@@ -1,31 +1,27 @@
-import os
-import openpyxl
-import sys
+import os, sys
 from pathlib import Path
 import pandas as pd
 import csv 
-from matplotlib import pyplot as plt 
+import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
+
 # Define CSV file 
-df = pd.read_csv("/home/aya/Desktop/Pheno_Work/analysis/Xsection_Test_20210817_2.csv")
+df = pd.read_csv("/home/aya/Pheno_Work/analysis/data_plots/Xsection_Test_20211020.csv")
 
 # Define coloumns in csv file in terms of x, y and z for plotting
 x = df['csqr_total']
 y = df['mbreaking']
-z = df['xsection(pb)']
+z = df['new_xsc(pb)']
 
-# plot data from file 
-
-# Define a figure for plotting
-fig = plt.figure(figsize=(7,7))
+# define figure for plotting 
+fig = plt.figure(num=None, figsize=(6,6), dpi=150, facecolor='white', edgecolor='white', frameon=False)
 my3dplot = fig.add_subplot(111, projection = '3d')
 
+# plot configuration
 my3dplot.scatter(x, y, z, marker = "o", color = "blue") 
-plt.xlabel(r"$\chi^2$", color="blue")
-plt.ylabel(r"$M12 x 10^2$", color="blue")
-my3dplot.set_zlabel(r"$\sigma(pb)(gg \rightarrow H \rightarrow hh \rightarrow bb4Mu) x 10^{-11}$", color="red")
+plt.xlabel(r"$\chi^2$", color="orange")
+plt.ylabel(r"$M_{12}$"+" x"+r"$10^{3}$", color="green")
+my3dplot.set_zlabel(r"$\sigma(gg\rightarrow$"+"H"+r"$\rightarrow$"+"hh"+r"$\rightarrow$"+"bb4Mu) x"+r"$10^{-8}$"+" (pb)", color="red")
 my3dplot.view_init(27)
-
-# way to add a legend
-#my3dplot.legend(loc='upper left')    # the contents of legend are define in label 
+plt.savefig("/home/aya/Pheno_Work/analysis/data_plots/newplots/3dplot.png")
